aab3c3776cd44c530ea4d9dfde50bebe8fb9ddb9
15zhoue <15zhoue@HS-280W-Z210-23.ab-hayward.net>
2015-02-27 10:46:44 -0500

    COMMMENTS

--- src/Line.java
+++ src/Line.java
@@ -1,25 +1,106 @@
+/*
+ * Kenny Wang, Anindita Yadavalli, Erica Zhou
+ * Mr. Marshmallow
+ * Java AP / Period 4
+ * 1 March 2015
+ */
+
+/*
+ * This class constructs a Line object and contains methods 
+ * to rotate it on a ShapePanel object
+ *
+ * This class extends the RotatingShape class
+ */
 
 public class Line extends RotatingShape {
 
+	/*
+	 * Constructs a Line object based on whether or not it is rotating,
+	 * whether it rotates clockwise or counterclockwise, its initial theta value,
+	 * and the amount theta changes per rotation
+	 * 
+	 * Parameters:
+	 *	boolean isRotating = whether or not the RotatingShape is rotating
+	 * 	boolean clockwise = whether RotatingShape rotates clockwise or counterclockwise
+	 *	double theta = current theta value 
+	 *	double deltaTheta = amount theta changes with each rotation
+	 */
+	
 	public Line(boolean isRotating, boolean clockwise, double theta, double delta){
 		super(isRotating, clockwise, theta, delta);
 	}
 
+	/*
+	 * Calculates the maximum radius (diagonal) of the line based on the
+	 * width and height of the panel on which it is drawn.
+	 * This allows the line to rotate via polar coordinates
+	 * 
+	 * Parameters:
+	 * 	int panelWidth = width of panel
+	 * 	int panelHeight = height of panel
+	 * 
+	 * Returns the maximum radius based on the panel
+	 */
+	
 	public double getRadius(int panelWidth, int panelHeight){
 		return Math.sqrt(panelWidth/2 * panelWidth/2 + panelHeight/2 * panelHeight/2);
 	}
+	
+	/*
+	 * Calculates the x-value of a point on one side of the line based
+	 * on the width and height of the panel on which it is drawn
+	 * 
+	 * Parameters:
+	 * 	int panelWidth = width of panel
+	 * 	int panelHeight = height of panel
+	 * 
+	 * Returns this x-coordinate
+	 */
 	
 	public int getX1(int panelWidth, int panelHeight){
 		return (int)(-getRadius(panelWidth, panelHeight) * Math.cos(getTheta()) + panelWidth/2);
 	}
 	
+	/*
+	 * Calculates the x-value of a point on the other side of the line based
+	 * on the width and height of the panel on which it is drawn
+	 * 
+	 * Parameters:
+	 * 	int panelWidth = width of panel
+	 * 	int panelHeight = height of panel
+	 * 
+	 * Returns this x-coordinate
+	 */
+	
 	public int getX2(int panelWidth, int panelHeight){
 		return (int)(getRadius(panelWidth, panelHeight) * Math.cos(getTheta()) + panelWidth/2);
 	}
 	
+	/*
+	 * Calculates the y-value of a point on one side of the line based
+	 * on the width and height of the panel on which it is drawn
+	 * 
+	 * Parameters:
+	 * 	int panelWidth = width of panel
+	 * 	int panelHeight = height of panel
+	 * 
+	 * Returns this y-coordinate
+	 */
+	
 	public int getY1(int panelWidth, int panelHeight){
 		return (int)(-getRadius(panelWidth, panelHeight) * Math.sin(getTheta()) + panelHeight/2);
 	}
+	
+	/*
+	 * Calculates the y-value of a point on the other side of the line based
+	 * on the width and height of the panel on which it is drawn
+	 * 
+	 * Parameters:
+	 * 	int panelWidth = width of panel
+	 * 	int panelHeight = height of panel
+	 * 
+	 * Returns this y-coordinate
+	 */
 	
 	public int getY2(int panelWidth, int panelHeight){
 		return (int)(getRadius(panelWidth, panelHeight) * Math.sin(getTheta()) + panelHeight/2);
--- src/RotatingShape.java
+++ src/RotatingShape.java
@@ -1,3 +1,22 @@
+/*
+ * Kenny Wang, Anindita Yadavalli, Erica Zhou
+ * Mr. Marshall
+ * Java AP / Period 4
+ * 1 March 2015
+ */
+
+/*
+ * This abstract class constructs a RotatingShape object and is 
+ * used as a superclass for the Line and RegularPolygon classes.
+ * It stores a theta value for the shape and rotates it by adjusting
+ * this value using polar coordinates.
+ * 
+ * Data includes:
+ * 	boolean isRotating = whether or not the RotatingShape is rotating
+ * 	boolean clockwise = whether RotatingShape rotates clockwise or counterclockwise
+ *	double theta = current theta value 
+ *	double deltaTheta = amount theta changes with each rotation
+ */
 
 public abstract class RotatingShape {
 
@@ -6,6 +25,18 @@ public abstract class RotatingShape {
 	private double theta;
 	private double deltaTheta;
 	
+	/*
+	 * Constructs a RotatingShape object based on whether or not it is rotating,
+	 * whether it rotates clockwise or counterclockwise, its initial theta value,
+	 * and the amount theta changes per rotation
+	 * 
+	 * Parameters:
+	 *	boolean isRotating = whether or not the RotatingShape is rotating
+	 * 	boolean clockwise = whether RotatingShape rotates clockwise or counterclockwise
+	 *	double theta = current theta value 
+	 *	double deltaTheta = amount theta changes with each rotation
+	 */
+	
 	public RotatingShape(boolean isRotating, boolean clockwise, double theta, double delta){
 		this.isRotating = isRotating;
 		this.clockwise = clockwise;
@@ -13,38 +44,87 @@ public abstract class RotatingShape {
 		this.deltaTheta = delta;
 	}
 	
+	/*
+	 * Returns the isRotating value of the RotatingShape object
+	 */
+	
 	public boolean isRotating(){
 		return isRotating;
 	}
 
+	/*
+	 * Sets the isRotating value of the RotatingShape object
+	 * 
+	 * Parameter:
+	 * 	boolean isRotating = whether or not the RotatingShape is rotating
+	 */
+	
 	public void setRotating(boolean isRotating){
 		this.isRotating = isRotating;
 	}
+	
+	/*
+	 * Returns the clockwise value of the RotatingShape object
+	 */
 	
 	public boolean getClockwise(){
 		return clockwise;
 	}
 	
+	/*
+	 * Sets the clockwise value of the RotatingShape object
+	 * 
+	 * Parameter:
+	 * 	boolean clockwise = whether RotatingShape rotates clockwise or counterclockwise
+	 */
+	
 	public void setClockwise(boolean clockwise){
 		this.clockwise = clockwise;
 	}
+	
+	/*
+	 * Returns the theta value of the RotatingShape object
+	 */
 	
 	public double getTheta(){
 		return theta;
 	}
 	
+	/*
+	 * Sets the theta value of the RotatingShape object
+	 * 
+	 * Parameter:
+	 * 	double theta = current theta value
+	 */
+	
 	public void setTheta(double theta){
 		this.theta = theta;
 	}
+	
+	/*
+	 * Returns the deltaTheta value of the RotatingShape object
+	 */
 	
 	public double getDelta(){
 		return deltaTheta;
 	}
 	
+	/*
+	 * Sets the deltaTheta value of the RotatingShape object
+	 * 
+	 * Parameter:
+	 * 	double deltaTheta = amount theta changes with each rotation
+	 */
+	
 	public void setDelta(double delta){
 		deltaTheta = delta;
 	}
 
+	/*
+	 * Rotates the RotatingShape object by adding or subtracting
+	 * deltaTheta depending on the value of clockwise
+	 */
+	
 	public void rotate(){
 		if(isRotating){
 			if(clockwise)
